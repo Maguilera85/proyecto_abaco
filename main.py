d3f7aa89fd824060a359187c7cfe4e96e43a1f75
claudio.caniullan <claudio.caniullan@usach.cl>
2020-11-10 16:14:20 -0300

    se agregan comentarios legibles

--- main.py
+++ main.py
@@ -2,10 +2,24 @@
 # numero es guardado en un diccionario
 numero = input('ingrese numero: ')
 dicc = dict(enumerate(numero))
+print(dicc)
+
+# ejemplo extraer digito del diccionario y trabajarlo
+# para imprimir en abaco
+
+unidad = int(dic[0])
+print(unidad)
+
 
 # numero es guardado en una lista
 lista = []
 lista.append(int(numero))
-
-print(dicc)
 print(lista)
+
+input()
+
+
+
+
+
+
