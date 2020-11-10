46f4a7b0bb77594dc7498ca32a6cb5205d8b876c
claudio.caniullan <claudio.caniullan@usach.cl>
2020-11-10 16:50:14 -0300

    primeras lineas de interaccion con usuario

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# numero es guardado en un diccionario
+numero = input('ingrese numero: ')
+dicc = dict(enumerate(numero))
+
+# numero es guardado en una lista
+lista = []
+lista.append(int(numero))
+
+print(dicc)
+print(lista)
