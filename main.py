1cde593b36fd7628ebe9c9102b9608abcd09ed1f
claudio.caniullan <claudio.caniullan@usach.cl>
2020-11-10 16:05:57 -0300

    revisan detalles de unidades

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ numero = input('ingrese numero: ')
 dicc = dict(enumerate(numero))
 print(dicc)
 
+unidad = int(dicc[5])
+decenas = int(dicc[4])
+centenas = int(dicc[3])
+
+print(unidad)
+
 # ejemplo extraer digito del diccionario y trabajarlo
 # para imprimir en abaco
 
+"""
 unidad = int(dic[0])
 print(unidad)
 
@@ -15,7 +22,7 @@ print(unidad)
 lista = []
 lista.append(int(numero))
 print(lista)
-
+"""
 input()
 
 
