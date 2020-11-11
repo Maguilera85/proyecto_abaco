7f8b8f1be4cb5a70703d4577d48e4ca9406fe471
Maguilera85 <drianketing@gmail.com>
2020-11-11 17:52:57 -0300

    se cambian coordenadas en abaco_dib

--- abaco_dib.py
+++ abaco_dib.py
@@ -1,14 +1,14 @@
 def imprimir_abaco(cm, dm, um, c, d, u):
     # Estas son las coordenadas del abaco
-    tablero = { 54: '   ' , 45: '   ' , 36: '   ' , 27: '   ' , ("d", 9): '   ' , ("u", 9): '   ' ,
-                53: '   ' , 44: '   ' , 35: '   ' , 26: '   ' , ("d", 8): '   ' , ("u", 8): '   ' ,
-                52: '   ' , 43: '   ' , 34: '   ' , 25: '   ' , ("d", 7): '   ' , ("u", 7): '   ' , 
-                51: '   ' , 42: '   ' , 33: '   ' , 24: '   ' , ("d", 6): '   ' , ("u", 6): '   ' ,
-                50: '   ' , 41: '   ' , 32: '   ' , 23: '   ' , ("d", 5): '   ' , ("u", 5): '   ' ,
-                49: '   ' , 40: '   ' , 31: '   ' , 22: '   ' , ("d", 4): '   ' , ("u", 4): '   ' ,
-                48: '   ' , 39: '   ' , 30: '   ' , 21: '   ' , ("d", 3): '   ' , ("u", 3): '   ' ,
-                47: '   ' , 38: '   ' , 29: '   ' , 20: '   ' , ("d", 2): '   ' , ("u", 2): '   ' ,
-                46: '   ' , 37: '   ' , 28: '   ' , 19: '   ' , ("d", 1): '   ' , ("u", 1): '   ' }
+    tablero = { ("cm", 9): '   ' , ("dm", 9): '   ' , ("um", 9): '   ' , ("c", 9): '   ' , ("d", 9): '   ' , ("u", 9): '   ' ,
+                ("cm", 8): '   ' , ("dm", 8): '   ' , ("um", 8): '   ' , ("c", 8): '   ' , ("d", 8): '   ' , ("u", 8): '   ' ,
+                ("cm", 7): '   ' , ("dm", 7): '   ' , ("um", 7): '   ' , ("c", 7): '   ' , ("d", 7): '   ' , ("u", 7): '   ' , 
+                ("cm", 6): '   ' , ("dm", 6): '   ' , ("um", 6): '   ' , ("c", 6): '   ' , ("d", 6): '   ' , ("u", 6): '   ' ,
+                ("cm", 5): '   ' , ("dm", 5): '   ' , ("um", 5): '   ' , ("c", 5): '   ' , ("d", 5): '   ' , ("u", 5): '   ' ,
+                ("cm", 4): '   ' , ("dm", 4): '   ' , ("um", 4): '   ' , ("c", 4): '   ' , ("d", 4): '   ' , ("u", 4): '   ' ,
+                ("cm", 3): '   ' , ("dm", 3): '   ' , ("um", 3): '   ' , ("c", 3): '   ' , ("d", 3): '   ' , ("u", 3): '   ' ,
+                ("cm", 2): '   ' , ("dm", 2): '   ' , ("um", 2): '   ' , ("c", 2): '   ' , ("d", 2): '   ' , ("u", 2): '   ' ,
+                ("cm", 1): '   ' , ("dm", 1): '   ' , ("um", 1): '   ' , ("c", 1): '   ' , ("d", 1): '   ' , ("u", 1): '   ' }
 
     posiciones = [("cm", cm), ("dm", dm), ("um", um), ("c", c), ("d", d), ("u", u)]
     for elemento in posiciones:
@@ -18,15 +18,15 @@ def imprimir_abaco(cm, dm, um, c, d, u):
     # imprime abaco visual
     print()
     print("  +---+       +---+       +---+       +---+       +---+       +---+")
-    print("  |" + tablero[54] + "|       |" + tablero[45] + "|       |" + tablero[36] + "|       |" + tablero[27] + "|       |" + tablero[("d", 9)] + "|       |" + tablero[("u", 9)] + "|")
-    print("  |" + tablero[53] + "|       |" + tablero[44] + "|       |" + tablero[35] + "|       |" + tablero[26] + "|       |" + tablero[("d", 8)] + "|       |" + tablero[("u", 8)] + "|")
-    print("  |" + tablero[52] + "|       |" + tablero[43] + "|       |" + tablero[34] + "|       |" + tablero[25] + "|       |" + tablero[("d", 7)] + "|       |" + tablero[("u", 7)] + "|")
-    print("  |" + tablero[51] + "|       |" + tablero[42] + "|       |" + tablero[33] + "|       |" + tablero[24] + "|       |" + tablero[("d", 6)] + "|       |" + tablero[("u", 6)] + "|")
-    print("  |" + tablero[50] + "|       |" + tablero[41] + "|       |" + tablero[32] + "|       |" + tablero[23] + "|       |" + tablero[("d", 5)] + "|       |" + tablero[("u", 5)] + "|")
-    print("  |" + tablero[49] + "|       |" + tablero[40] + "|       |" + tablero[31] + "|       |" + tablero[22] + "|       |" + tablero[("d", 4)] + "|       |" + tablero[("u", 4)] + "|")
-    print("  |" + tablero[48] + "|       |" + tablero[39] + "|       |" + tablero[30] + "|       |" + tablero[21] + "|       |" + tablero[("d", 3)] + "|       |" + tablero[("u", 3)] + "|")
-    print("  |" + tablero[47] + "|       |" + tablero[38] + "|       |" + tablero[29] + "|       |" + tablero[20] + "|       |" + tablero[("d", 2)] + "|       |" + tablero[("u", 2)] + "|")
-    print("  |" + tablero[46] + "|       |" + tablero[37] + "|       |" + tablero[28] + "|       |" + tablero[19] + "|       |" + tablero[("d", 1)] + "|       |" + tablero[("u", 1)] + "|")
+    print("  |" + tablero[("cm", 9)] + "|       |" + tablero[("dm", 9)] + "|       |" + tablero[("um", 9)] + "|       |" + tablero[("c", 9)] + "|       |" + tablero[("d", 9)] + "|       |" + tablero[("u", 9)] + "|")
+    print("  |" + tablero[("cm", 8)] + "|       |" + tablero[("dm", 8)] + "|       |" + tablero[("um", 8)] + "|       |" + tablero[("c", 8)] + "|       |" + tablero[("d", 8)] + "|       |" + tablero[("u", 8)] + "|")
+    print("  |" + tablero[("cm", 7)] + "|       |" + tablero[("dm", 7)] + "|       |" + tablero[("um", 7)] + "|       |" + tablero[("c", 7)] + "|       |" + tablero[("d", 7)] + "|       |" + tablero[("u", 7)] + "|")
+    print("  |" + tablero[("cm", 6)] + "|       |" + tablero[("dm", 6)] + "|       |" + tablero[("um", 6)] + "|       |" + tablero[("c", 6)] + "|       |" + tablero[("d", 6)] + "|       |" + tablero[("u", 6)] + "|")
+    print("  |" + tablero[("cm", 5)] + "|       |" + tablero[("dm", 5)] + "|       |" + tablero[("um", 5)] + "|       |" + tablero[("c", 5)] + "|       |" + tablero[("d", 5)] + "|       |" + tablero[("u", 5)] + "|")
+    print("  |" + tablero[("cm", 4)] + "|       |" + tablero[("dm", 4)] + "|       |" + tablero[("um", 4)] + "|       |" + tablero[("c", 4)] + "|       |" + tablero[("d", 4)] + "|       |" + tablero[("u", 4)] + "|")
+    print("  |" + tablero[("cm", 3)] + "|       |" + tablero[("dm", 3)] + "|       |" + tablero[("um", 3)] + "|       |" + tablero[("c", 3)] + "|       |" + tablero[("d", 3)] + "|       |" + tablero[("u", 3)] + "|")
+    print("  |" + tablero[("cm", 2)] + "|       |" + tablero[("dm", 2)] + "|       |" + tablero[("um", 2)] + "|       |" + tablero[("c", 2)] + "|       |" + tablero[("d", 2)] + "|       |" + tablero[("u", 2)] + "|")
+    print("  |" + tablero[("cm", 1)] + "|       |" + tablero[("dm", 1)] + "|       |" + tablero[("um", 1)] + "|       |" + tablero[("c", 1)] + "|       |" + tablero[("d", 1)] + "|       |" + tablero[("u", 1)] + "|")
     print("__+---+_______+---+_______+---+_______+---+_______+---+_______+---+__")
     print("|___________________________________________________________________|")
     print()
