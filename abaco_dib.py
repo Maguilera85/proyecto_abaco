4cc3e1a0976fb86bf9ffe336e7fb7ad32cc2f401
ljaraque <ljaraque@yahoo.com>
2020-11-11 17:19:01 -0300

    ideas de imprimir

--- abaco_dib.py
+++ abaco_dib.py
@@ -1,29 +1,37 @@
-# Estas son las coordenadas del abaco
-tablero = {54: '   ' , 45: '   ' , 36: '   ' , 27: '   ' , 18: '   ' , 9: '   ' ,
-            53: '   ' , 44: '   ' , 35: '   ' , 26: '   ' , 17: '   ' , 8: '   ' ,
-            52: '   ' , 43: '   ' , 34: '   ' , 25: '   ' , 16: '   ' , 7: '   ' , 
-            51: '   ' , 42: '   ' , 33: '   ' , 24: '   ' , 15: '   ' , 6: '   ' ,
-            50: '   ' , 41: '   ' , 32: '   ' , 23: '   ' , 14: '   ' , 5: '   ' ,
-            49: '   ' , 40: '   ' , 31: '   ' , 22: '   ' , 13: '   ' , 4: '   ' ,
-            48: '   ' , 39: '   ' , 30: '   ' , 21: '   ' , 12: '   ' , 3: '   ' ,
-            47: '   ' , 38: '   ' , 29: '   ' , 20: '   ' , 11: '   ' , 2: '   ' ,
-            46: '   ' , 37: '   ' , 28: '   ' , 19: '   ' , 10: '   ' , 1: '   ' }
-           
-# imprime abaco visual
-print()
-print("  +---+       +---+       +---+       +---+       +---+       +---+")
-print("  |" + tablero[54] + "|       |" + tablero[45] + "|       |" + tablero[36] + "|       |" + tablero[27] + "|       |" + tablero[18] + "|       |" + tablero[9] + "|")
-print("  |" + tablero[53] + "|       |" + tablero[44] + "|       |" + tablero[35] + "|       |" + tablero[26] + "|       |" + tablero[17] + "|       |" + tablero[8] + "|")
-print("  |" + tablero[52] + "|       |" + tablero[43] + "|       |" + tablero[34] + "|       |" + tablero[25] + "|       |" + tablero[16] + "|       |" + tablero[7] + "|")
-print("  |" + tablero[51] + "|       |" + tablero[42] + "|       |" + tablero[33] + "|       |" + tablero[24] + "|       |" + tablero[15] + "|       |" + tablero[6] + "|")
-print("  |" + tablero[50] + "|       |" + tablero[41] + "|       |" + tablero[32] + "|       |" + tablero[23] + "|       |" + tablero[14] + "|       |" + tablero[5] + "|")
-print("  |" + tablero[49] + "|       |" + tablero[40] + "|       |" + tablero[31] + "|       |" + tablero[22] + "|       |" + tablero[13] + "|       |" + tablero[4] + "|")
-print("  |" + tablero[48] + "|       |" + tablero[39] + "|       |" + tablero[30] + "|       |" + tablero[21] + "|       |" + tablero[12] + "|       |" + tablero[3] + "|")
-print("  |" + tablero[47] + "|       |" + tablero[38] + "|       |" + tablero[29] + "|       |" + tablero[20] + "|       |" + tablero[11] + "|       |" + tablero[2] + "|")
-print("  |" + tablero[46] + "|       |" + tablero[37] + "|       |" + tablero[28] + "|       |" + tablero[19] + "|       |" + tablero[10] + "|       |" + tablero[1] + "|")
-print("__+---+_______+---+_______+---+_______+---+_______+---+_______+---+__")
-print("|___________________________________________________________________|")
-print()
+def imprimir_abaco(cm, dm, um, c, d, u):
+    # Estas son las coordenadas del abaco
+    tablero = { 54: '   ' , 45: '   ' , 36: '   ' , 27: '   ' , ("d", 9): '   ' , ("u", 9): '   ' ,
+                53: '   ' , 44: '   ' , 35: '   ' , 26: '   ' , ("d", 8): '   ' , ("u", 8): '   ' ,
+                52: '   ' , 43: '   ' , 34: '   ' , 25: '   ' , ("d", 7): '   ' , ("u", 7): '   ' , 
+                51: '   ' , 42: '   ' , 33: '   ' , 24: '   ' , ("d", 6): '   ' , ("u", 6): '   ' ,
+                50: '   ' , 41: '   ' , 32: '   ' , 23: '   ' , ("d", 5): '   ' , ("u", 5): '   ' ,
+                49: '   ' , 40: '   ' , 31: '   ' , 22: '   ' , ("d", 4): '   ' , ("u", 4): '   ' ,
+                48: '   ' , 39: '   ' , 30: '   ' , 21: '   ' , ("d", 3): '   ' , ("u", 3): '   ' ,
+                47: '   ' , 38: '   ' , 29: '   ' , 20: '   ' , ("d", 2): '   ' , ("u", 2): '   ' ,
+                46: '   ' , 37: '   ' , 28: '   ' , 19: '   ' , ("d", 1): '   ' , ("u", 1): '   ' }
+
+    posiciones = [("cm", cm), ("dm", dm), ("um", um), ("c", c), ("d", d), ("u", u)]
+    for elemento in posiciones:
+        for idx in range(1, elemento[1] + 1):
+            tablero[(elemento[0], idx)] = "XXX"
+
+    # imprime abaco visual
+    print()
+    print("  +---+       +---+       +---+       +---+       +---+       +---+")
+    print("  |" + tablero[54] + "|       |" + tablero[45] + "|       |" + tablero[36] + "|       |" + tablero[27] + "|       |" + tablero[("d", 9)] + "|       |" + tablero[("u", 9)] + "|")
+    print("  |" + tablero[53] + "|       |" + tablero[44] + "|       |" + tablero[35] + "|       |" + tablero[26] + "|       |" + tablero[("d", 8)] + "|       |" + tablero[("u", 8)] + "|")
+    print("  |" + tablero[52] + "|       |" + tablero[43] + "|       |" + tablero[34] + "|       |" + tablero[25] + "|       |" + tablero[("d", 7)] + "|       |" + tablero[("u", 7)] + "|")
+    print("  |" + tablero[51] + "|       |" + tablero[42] + "|       |" + tablero[33] + "|       |" + tablero[24] + "|       |" + tablero[("d", 6)] + "|       |" + tablero[("u", 6)] + "|")
+    print("  |" + tablero[50] + "|       |" + tablero[41] + "|       |" + tablero[32] + "|       |" + tablero[23] + "|       |" + tablero[("d", 5)] + "|       |" + tablero[("u", 5)] + "|")
+    print("  |" + tablero[49] + "|       |" + tablero[40] + "|       |" + tablero[31] + "|       |" + tablero[22] + "|       |" + tablero[("d", 4)] + "|       |" + tablero[("u", 4)] + "|")
+    print("  |" + tablero[48] + "|       |" + tablero[39] + "|       |" + tablero[30] + "|       |" + tablero[21] + "|       |" + tablero[("d", 3)] + "|       |" + tablero[("u", 3)] + "|")
+    print("  |" + tablero[47] + "|       |" + tablero[38] + "|       |" + tablero[29] + "|       |" + tablero[20] + "|       |" + tablero[("d", 2)] + "|       |" + tablero[("u", 2)] + "|")
+    print("  |" + tablero[46] + "|       |" + tablero[37] + "|       |" + tablero[28] + "|       |" + tablero[19] + "|       |" + tablero[("d", 1)] + "|       |" + tablero[("u", 1)] + "|")
+    print("__+---+_______+---+_______+---+_______+---+_______+---+_______+---+__")
+    print("|___________________________________________________________________|")
+    print()
+
+imprimir_abaco(1, 1, 1, 1, 1, 1)
 
 
 # descompone el numero ingresado por el usuario
