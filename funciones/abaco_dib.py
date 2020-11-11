ce768c701ee1b478e5adcc772a1a27355b2e7532
sezniv <sezniv@gmail.com>
2020-11-10 22:59:41 -0300

    Avance Funcion

--- funciones/abaco_dib.py
+++ funciones/abaco_dib.py
@@ -1,9 +1,24 @@
+theBoard = {'cm9': ' ' , 'dm9': ' ' , 'um9': ' ' , 'c9': ' ' , 'd9': ' ' , 'u9': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um8': ' ' , 'c8': ' ' , 'd8': ' ' , 'u8': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um7': ' ' , 'c7': ' ' , 'd7': ' ' , 'u7': ' ' , 
+            'cm9': ' ' , 'dm9': ' ' , 'um6': ' ' , 'c6': ' ' , 'd6': ' ' , 'u6': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um5': ' ' , 'c5': ' ' , 'd5': ' ' , 'u5': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um4': ' ' , 'c4': ' ' , 'd4': ' ' , 'u4': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um3': ' ' , 'c3': ' ' , 'd3': ' ' , 'u3': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um2': ' ' , 'c2': ' ' , 'd2': ' ' , 'u2': ' ' ,
+            'cm9': ' ' , 'dm9': ' ' , 'um1': ' ' , 'c1': ' ' , 'd1': ' ' , 'u1': ' ' ,}
 
+board_keys = []
 
-variablex = "XXX"
-variable0 = "   "
+for key in theBoard:
+    board_keys.append(key)
 
-
+"""– 1 unidad = 1 unidad
+   – 1 decena = 10 unidades
+   – 1 centena = 100 unidades
+   – 1 unidad de mil = 1 000 unidades
+   – 1 decena de mil = 10 000 unidades
+   – 1 centena de mil = 100 000 unidades """
 
 print()
 print("  +---+       +---+       +---+       +---+       +---+       +---+")
---
+++ funciones/graficarNumero.py
@@ -0,0 +1,12 @@
+def graficarNumero(numero_walter):
+  numero_walter = valor_str
+  valor_str = len(str(valor))
+  if valor_str == "8":
+    cadena = "X"
+    multiplicador_x = numero_walter / 100000
+    imprimir_pantalla = cadena * multiplicador_x
+    restante = valor - 
+      str_restante = len(restante)
+
+
+  print(valor_str)
