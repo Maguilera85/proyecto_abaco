2da8e0245aa327ba3ed5cf2e4d86c44127752147
claudio.caniullan <claudio.caniullan@usach.cl>
2020-11-10 22:08:06 -0300

    se mezclan codigos

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-
-# numero es guardado en un diccionario
-numero = input('ingrese numero: ')
-dicc = dict(enumerate(numero))
-print(dicc)
-
-# ejemplo extraer digito del diccionario y trabajarlo
-# para imprimir en abaco
-
-unidad = int(dicc[0])
-print(unidad)
-
-
-# numero es guardado en una lista
 lista = []
-numero1 = int(numero)
-while numero1 > 0:
-	lista.append(numero1)
+
+while True:
+	# se gurdan digitos en diccionario
+	numero = input('ingrese numero: ')
+	dicc = dict(enumerate(numero))
+	# aqui se ejecuta la funcion graficar
+	print(dicc)
+     
+    #se guarda numeros en lista
+	lista.append(numero)
+	r = input('Desea salir: si o no:')
+
+	if r == 'no':
+		continue
+	else:
+		break
 
 print(lista)
 
-
-
-
-
-
-
+for i in range(len(lista)):
+	print(lista[i])
 
 input()
-
-
-
 
 
 
